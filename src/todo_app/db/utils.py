25bc79909fc0aaec67ca037300e1128e427db51c
Eyad Jawad <ayadmansr@gmail.com>
2026-08-31 22:23:39 +0300

    Separate files and more polish

---
+++ src/todo_app/db/utils.py
@@ -0,0 +1,34 @@
+from datetime import datetime, UTC
+
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from todo_app.db.models import Todo
+
+
+async def get_todos(session: AsyncSession, user_id: int) -> list[Todo]:
+    stmt = select(Todo).where(Todo.user_id == user_id)
+    result = await session.execute(stmt)
+    return result.scalars().all()
+
+
+async def add_todo(user_id: int, session: AsyncSession) -> None:
+    text = input("Please input the todo text:\n")
+
+    todo = Todo(
+        user_id=user_id, 
+        todo_text=text, 
+        creation_date=datetime.now(tz=UTC), 
+        is_done=False,
+    )
+
+    session.add(todo)
+    await session.commit()
+
+
+async def delete_todo(session: AsyncSession, todo: Todo) -> None:
+    session.delete(todo)
+    await session.commit()
+
+async def toggle_todo(session: AsyncSession, todo: Todo) -> None:
+    todo.is_done = not todo.is_done
+    await session.commit()
--- src/todo_app/ui.py
+++ src/todo_app/ui.py
@@ -1,14 +1,18 @@
 import readchar
 
 from textwrap import dedent
-from datetime import datetime, UTC
 from enum import Enum, auto
 
 from rich.console import Console
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from todo_app.db import init_db, get_session
 from todo_app.db.models import Todo
+from todo_app.db.utils import (
+    get_todos, 
+    add_todo, 
+    delete_todo, 
+    toggle_todo,
+)
 
 class KeyAction(Enum):
     NOTHING = auto()
@@ -21,6 +25,7 @@ CONTROLS_STR = dedent("""
     Press:
         [cyan](Enter)[/] to toggle the todo
         [cyan](A)[/]dd to add a new todo
+        [cyan]Arrow keys[/] to navigate the todos
         [red](D)[/]elete to delete a highlighted todo
         [red](Q)[/]uit to quit the app
 """)
@@ -61,12 +66,11 @@ async def interface(user_id: int = 0):
             console.print(format_todos(todos, current_line, console))
 
 
-async def handle_keys(todos: list[Todo], current_line: int, session: AsyncSession, user_id: int):
+async def handle_keys(todos: list[Todo], current_line: int, session: AsyncSession, user_id: int) -> tuple[int, int]:
     key = readchar.readkey()
 
     if key == readchar.key.ENTER:
-        todos[current_line].is_done = not todos[current_line].is_done
-        await session.commit()
+        await toggle_todo(session, todos[current_line])
         return current_line, KeyAction.DB_CHANGED
     elif key == readchar.key.DOWN:
         current_line = move_cursor(current_line, 1, len(todos))
@@ -105,6 +109,7 @@ def format_todos(todos: list[Todo], current_line: int, console: Console) -> str:
 def todo_symbol_maker(todo: Todo) -> str:
     return FILLED_SQUARE if todo.is_done else EMPTY_SQUARE
 
+
 def select_window(todos: list[Todo], current_idx: int, window_length: int) -> list[str]:
     n = len(todos)
 
@@ -138,35 +143,10 @@ def is_str_fit_terminal(s: str, console: Console) -> bool:
     if console.height < s.count('\n'):
         return False
 
-    console_size = console.height * console.size.width
+    console_size = console.height * console.width
 
     return len(s) < console_size
 
 
 def move_cursor(current_line: int, direction: int, length: int) -> int:
     return (current_line + direction) % length
-
-
-async def get_todos(session: AsyncSession, user_id: int) -> list[Todo]:
-    stmt = select(Todo).where(Todo.user_id == user_id)
-    result = await session.execute(stmt)
-    return result.scalars().all()
-
-
-async def add_todo(user_id: int, session: AsyncSession) -> None:
-    text = input("Please input the todo text:\n")
-
-    todo = Todo(
-        user_id=user_id, 
-        todo_text=text, 
-        creation_date=datetime.now(tz=UTC), 
-        is_done=False,
-    )
-
-    session.add(todo)
-    await session.commit()
-
-
-async def delete_todo(session: AsyncSession, todo: Todo) -> None:
-    session.delete(todo)
-    await session.commit()
